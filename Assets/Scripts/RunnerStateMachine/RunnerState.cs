0210ec5eafd090688b445fe57386e60d03ac9cd3
Métaphore <raf.duhamel@gmail.com>
2024-01-18 17:58:23 -0500

    +Added Raf-Sandbox scene +Added RunnerStateMachine hierachy

---
+++ Assets/Scripts/RunnerStateMachine/RunnerState.cs
@@ -0,0 +1,40 @@
+public abstract class CharacterState : IState
+{
+    protected RunnerControllerStateMachine m_stateMachine;
+
+    public void OnStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public virtual void OnStart(RunnerControllerStateMachine stateMachineRef)
+    {
+        m_stateMachine = stateMachineRef;
+    }
+
+    public virtual void OnEnter()
+    {
+    }
+
+    public virtual void OnExit()
+    {
+    }
+
+    public virtual void OnFixedUpdate()
+    {
+    }
+
+    public virtual void OnUpdate()
+    {
+    }
+
+    public virtual bool CanEnter(IState currentState)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public virtual bool CanExit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
