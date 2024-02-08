af18d5a326bcecb6d15bcb5d3ccc50cc93f883ac
thebarpon <poroy995@gmail.com>
2024-02-08 18:26:10 -0500

    *modified trap manager

--- Assets/Scripts/HunterTools/TrapManager.cs
+++ Assets/Scripts/HunterTools/TrapManager.cs
@@ -1,18 +1,50 @@
-using System.Collections;
+using Mirror;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrapManager : MonoBehaviour
+public class TrapManager : NetworkBehaviour
 {
+    [SerializeField]
+    private List<FlamethrowerActivation> m_flamethrowerSerieOne = new List<FlamethrowerActivation>();
+    [SerializeField]
+    private List<FlamethrowerActivation> m_flamethrowerSerieTwo = new List<FlamethrowerActivation>();
+    [SerializeField]
+    private List<FlamethrowerActivation> m_flamethrowerSeriethree = new List<FlamethrowerActivation>();
+    [SerializeField]
+    private List<TrapDoorController> m_trapDoorSerieOne = new List<TrapDoorController>();
+    [SerializeField]
+    private List<TrapDoorController> m_TrapDoorSerieTwo = new List<TrapDoorController>();
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnFTSet1()
+    {
+        if (isClient)
+        {
+            foreach (FlamethrowerActivation flamethrower in m_flamethrowerSerieOne)
+            {
+                //flamethrower.CommandActivatedEffect();
+            }
+        }
+    }
+
+    public void OnFTSet2()
+    {
+        if (isClient)
+        {
+            foreach (FlamethrowerActivation flamethrower in m_flamethrowerSerieTwo)
+            {
+                //flamethrower.CommandActivatedEffect();
+            }
+        }
     }
 }
