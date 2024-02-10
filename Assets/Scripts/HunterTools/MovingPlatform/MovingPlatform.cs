a9c5c2ae965a4f24ebe9a8f2425d8a3674ec19c6
thebarpon <poroy995@gmail.com>
2024-02-10 13:48:57 -0500

    -Removed moving plateform system *Fixed moving platform to work as intended

--- Assets/Scripts/HunterTools/MovingPlatform/MovingPlatform.cs
+++ Assets/Scripts/HunterTools/MovingPlatform/MovingPlatform.cs
@@ -1,30 +1,29 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    [Serialize]
+    [SerializeField]
+    private Transform initialPosition;
+    [SerializeField]
     public List<Transform> movingCoordinate;
     [SerializeField]
     private float speed;
 
-
     private int coordinateIndex;
     private int lastCoordinateIndex;
-    private Vector3 initialPosition;
     private bool isInReverse;
-    private Vector3 nextCoordinate;
+
+    const float TRANSFORM_MARGIN = 0.001f;
 
     // Start is called before the first frame update
     void Start()
     {
-        coordinateIndex = 0;
-        lastCoordinateIndex = movingCoordinate.Count - 1;
+        coordinateIndex = 1;
         isInReverse = false;
-        initialPosition = transform.position;
+        movingCoordinate.Insert(0, initialPosition);
+        lastCoordinateIndex = movingCoordinate.Count - 1;
         //Debug.Log(movingCoordinate[0].position);
-        nextCoordinate = movingCoordinate[coordinateIndex].position;
     }
 
     void Update()
@@ -32,46 +31,91 @@ public class MovingPlatform : MonoBehaviour
         //Debug.Log("Update platform");
         UpdatePosition();
 
-        if (transform.position == nextCoordinate)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log("Platform position " + transform.position);
+            Debug.Log("Platform locaposition " + transform.localPosition);
+            Debug.Log("Moving position " + movingCoordinate[coordinateIndex].position);
+            Debug.Log("moving localposition " + movingCoordinate[coordinateIndex].localPosition);
+        }
+
+        if (Vector3.Distance(transform.position, movingCoordinate[coordinateIndex].position) < TRANSFORM_MARGIN)
+        {
+            Debug.Log("Reached destination");
             if (!isInReverse)
             {
                 if (coordinateIndex != lastCoordinateIndex)
                 {
+                    //Debug.Log("Go up one coordinate");
                     coordinateIndex++;
-                    nextCoordinate = movingCoordinate[coordinateIndex].position;
                 }
                 else
                 {
+                    //Debug.Log("Return to origine");
                     isInReverse = true;
                     coordinateIndex--;
-                    nextCoordinate = movingCoordinate[coordinateIndex].position;
                 }
+                return;
             }
 
             if (isInReverse)
             {
-                if (nextCoordinate == movingCoordinate[0].position)
+                if (coordinateIndex > 0)
                 {
-                    nextCoordinate = initialPosition;
-                }
-                else if (nextCoordinate != initialPosition)
-                {
+                    //Debug.Log("Go down one coordinate");
                     coordinateIndex--;
-                    nextCoordinate = movingCoordinate[coordinateIndex].position;
                 }
                 else
                 {
+                    //Debug.Log("Reset coordinate order");
                     isInReverse = false;
-                    coordinateIndex = 0;
-                    nextCoordinate = movingCoordinate[coordinateIndex].position;
+                    coordinateIndex = 1;
                 }
             }
+            Debug.Log(coordinateIndex);
         }
+
+        //if (transform.position == nextCoordinate)
+        //{
+        //    if (!isInReverse)
+        //    {
+        //        if (coordinateIndex != lastCoordinateIndex)
+        //        {
+        //            coordinateIndex++;
+        //            nextCoordinate = movingCoordinate[coordinateIndex].position;
+        //        }
+        //        else
+        //        {
+        //            isInReverse = true;
+        //            coordinateIndex--;
+        //            nextCoordinate = movingCoordinate[coordinateIndex].position;
+        //        }
+        //    }
+
+        //    if (isInReverse)
+        //    {
+        //        if (nextCoordinate == movingCoordinate[0].position)
+        //        {
+        //            nextCoordinate = initialPosition.position;
+        //        }
+        //        else if (nextCoordinate != initialPosition.position)
+        //        {
+        //            coordinateIndex--;
+        //            nextCoordinate = movingCoordinate[coordinateIndex].position;
+        //        }
+        //        else
+        //        {
+        //            isInReverse = false;
+        //            coordinateIndex = 0;
+        //            nextCoordinate = movingCoordinate[coordinateIndex].position;
+        //        }
+        //    }
+        //}
     }
 
     private void UpdatePosition()
     {
-        transform.position = Vector3.MoveTowards(transform.position, nextCoordinate, speed * Time.fixedDeltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, nextCoordinate, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, movingCoordinate[coordinateIndex].position, speed * Time.deltaTime);
     }
 }
