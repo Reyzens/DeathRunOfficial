0a646ddfe35188529b49e5ec8d2dbf21cf90a308
DESKTOP-E2VEPC1\rafdu <raf.duhamel@gmail.com>
2024-02-20 01:47:40 -0500

    *Can now respawn at last checkpoint after dying

--- Assets/Scripts/RunnerStateMachine/DeadState.cs
+++ Assets/Scripts/RunnerStateMachine/DeadState.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DeadState : RunnerState
 {
     private const float STATE_EXIT_TIMER = 1.5f;
+    CheckpointManager m_checkpointManager;
     private float m_currentStateTimer;
     public override void OnEnter()
     {
@@ -12,12 +14,15 @@ public class DeadState : RunnerState
         m_currentStateTimer = STATE_EXIT_TIMER;
         m_stateMachine.Animator.SetTrigger("Dead");
         m_stateMachine.Animator.SetBool("Alive", false);
+
+        m_checkpointManager = GameObject.Find("CheckpointManager").GetComponent<CheckpointManager>();
     }
 
     public override void OnExit()
     {
         m_stateMachine.Animator.SetBool("Alive", true);
         m_stateMachine.m_isAlive = true;
+        m_stateMachine.transform.position = m_checkpointManager.m_lastCheckpoint;
     }
 
     public override void OnFixedUpdate()
